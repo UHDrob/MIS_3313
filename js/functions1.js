81626bdea9edf3673ccec5417638022c97fbabb4
Roberto Gomez <roberto77@gmail.com>
2022-04-25 18:33:40 -0500

    final update

---
+++ js/functions1.js
@@ -0,0 +1,33 @@
+/* add in your functions here */
+function outputCountries() {
+    for (i=0; i<countries.length; i++) {
+       document.write(
+          "<div class='item'>" +
+       "<h2>" +  countries[i].country + "</h2>"  + 
+         "<p>" + 
+        countries[i].continent 
+        + "</p> <p class='inner-box'> <strong> Cities</strong> <br>" 
+       );
+       outputCountryBox(countries[i]);
+       document.write("  </div>")
+    }
+ }
+ 
+ function outputCountryBox(city) {
+    for (j=0; j<city.cities.length;j++) {
+       document.write(
+         city.cities[j].city  + "<br>");
+    }
+    document.write( "</p>" +  
+      "<p class='inner-box'><strong>Popular Photos</strong></p>");
+      for (k=0; k<city.photos.length;k++) {
+         document.write( "<img class='photo' src='" +
+            city.photos[k].source
+             + "'>");
+      }  
+    document.write(  
+      "<button>Visit</button>    <br>" );
+      
+ }
+ 
+ outputCountries();
