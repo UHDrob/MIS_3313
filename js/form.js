39b5050e48f66f2f7e41df095c514def4a7cf191
Roberto Gomez <roberto77@gmail.com>
2022-05-05 21:53:35 -0500

    form

--- js/form.js
+++ js/form.js
@@ -1,10 +1,29 @@
-funtion validate() {
 
-    let x = document.forms["mainForm"]["customer"].value;
-    if (name == "") {
+document.getElementById("loginForm").addEventListener("submit", function(e) {
+    var noName = document.getElementById("customer").value;
+    var noCountry = document.getElementById("countries").value;
+    var noStatus = document.getElementById("status").value;
+
+    if (noName == null || noName == "") {
+        // the fiedl was empty. Stop the form submission
+        e.preventDefault();
+        // Now tell teh user somthing went wrong
         alert("Please enter a name");
-        return false;
+    };
+
+    if (noCountry == 0 {
+        e.preventDefault();
+        // Now tell teh user somthing went wrong
+        alert("Please choose a country");
+    });
+
+    if (noStatus != 0 && noStatus != 1  && noStatus != 2) {
+        e.preventDefault();
+        // Now tell teh user somthing went wrong
+        alert("Please choose a stastua");
     }
-};
 
 
+
+});
+        
