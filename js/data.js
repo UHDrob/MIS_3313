766cf05ae8b997cef572dd874b704211581a81d9
Roberto Gomez <roberto77@gmail.com>
2022-05-07 18:12:06 -0500

    homwrork 13

---
+++ js/data.js
@@ -0,0 +1,6 @@
+/* arrays */
+
+var filenames = ["106020.jpg", "116010.jpg", "120010.jpg"];
+var titles = ["Girld with a Pearl Earring", "Artist Holding a Thistle", "Portrait of Eleanor of Toledo"];
+var quantities = [3,1,2];
+var prices = [80, 125, 75];
